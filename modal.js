b4cf40cbdf1b94f6a1dcfc0d1d7b3e7e2c6eca1d
SledgeDew <sledgelozon@gmail.com>
2024-01-31 23:41:35 +0100

    js_update

--- modal.js
+++ modal.js
@@ -46,23 +46,24 @@ form.addEventListener("submit", (event) => {
 
   // Récupérer, écouter et vérifier l'entrée du Prénom
   let baliseFirstName = document.getElementById("first")
-  let first = baliseFirstName.value
-  function validName(name) {
-    return name.length >= 2
+  let firstname = baliseFirstName.value
+  function validName(firstname) {
+    return firstname.length >= 2
   }
 
-  if (validName(first)) {
-    document.getElementById("name_error").innerText = "" // Efface le message d'erreur s'il y en a un
+  if (validName(firstname)) {
+    document.getElementById("firstname_error").innerText = "" // Efface le message d'erreur s'il y en a un
+    console.log("Le Prenom est valide.")
    
     baliseFirstName.classList.remove("error")
  
-  } else if(first===null || first==""){
-    document.getElementById("name_error").innerText = "Veuillez renseigner le champ" // Efface le message d'erreur s'il y en a un
-    console.log("Le prénom est valide.")
+  } else if(firstname===null || firstname==""){
+    document.getElementById("firstname_error").innerText = "Veuillez renseigner le champ" // Efface le message d'erreur s'il y en a un
+    console.log("Veuillez renseigner le champ prenom.")
     baliseFirstName.classList.remove("error")
   } else {
     // Affiche un message d'erreur dans le span portant l'id "name_error"
-    document.getElementById("name_error").innerText = "Veuillez saisir au moins 2 caractères."
+    document.getElementById("firstname_error").innerText = "Veuillez saisir au moins 2 caractères."
     
     // Ajoute la classe error
     baliseFirstName.classList.add("error")
@@ -80,7 +81,7 @@ if (validName(last)) {
 
  else if(last===null || last==""){
   document.getElementById("lastname_error").innerText = "Veuillez renseigner le champ" // Efface le message d'erreur s'il y en a un
-  console.log("Le prénom est valide.")
+  console.log("Veuillez renseigner le champ Nom.")
   baliseFirstName.classList.remove("error")
 }
 else {
@@ -103,6 +104,7 @@ else {
  }
  if (validEmail(email)) {
    document.getElementById("email_error").innerText = "" // Efface le message d'erreur s'il y en a un
+   console.log("Le mail est valide.")
    
    baliseEmail.classList.remove("error")
  } 
@@ -168,10 +170,11 @@ for (let index = 0; index < listBtnRadioArray.length; index++) {
 if (!isRadioButtonChecked) {
   // Affiche un message d'erreur dans la div portant l'id "cityError"
   document.getElementById("location_error").innerText = "Veuillez sélectionner une ville."
-  console.error("Veuillez sélectionner une ville.")
+  console.log("Veuillez sélectionner une ville.")
 
   for (let index = 0; index < listBtnRadioArray.length; index++) {
     // Supprime la classe checkbox-label
+    
     listBtnRadioArray[index].classList.remove("checkbox-label")
 
     // Ajoute la classe error-checkbox
@@ -189,10 +192,46 @@ let isCheckboxConditionsChecked = checkboxConditions.checked
 
 if (isCheckboxConditionsChecked) {
   document.getElementById("checkbox_error").innerText = ""
+  console.log("La condition est valide .")
+
 } else {
   // Affiche un message d'erreur dans le span portant l'id "checkbox_error"
   document.getElementById("checkbox_error").innerText = "Veuillez accepter les conditions d'utilisation."
 }
+
+// Dernière étape : Vérification du formulaire et affichage de la fenêtre de remerciement
+  // Affichage de la fênetre de confirmation du formulaire
+
+  // Vérifie si tous les champs sont valides
+  if (
+    validName(first)
+    && validName(last)
+    && validEmail(email)
+    && baliseNumberParticipation(quantity)
+    && isRadioButtonChecked
+    && isCheckboxConditionsChecked
+  ) {
+    console.log("Prénom : ", baliseFirstName.value)
+    console.log("Nom : ", baliseLastName.value)
+    console.log("Email : ", baliseEmail.value)
+    console.log("Nombre de participation : ", baliseNumberParticipation.value)
+    console.log("Les conditions générales d'utilisation sont acceptés.")
+
+    form.reset()
+    form.style.display = "none"
+
+    let messageThanks = document.querySelector(".message")
+    messageThanks.style.display = "flex"
+
+    document.querySelector("message button")
+        .addEventListener("click", () => {
+          modalbg.style.display = "none"
+          form.style.display = "block"
+          messageThanks.style.display = "none"
+        })
+  } else {
+    console.log("Certains champs ne sont pas valides")
+  }
 
 
 })
